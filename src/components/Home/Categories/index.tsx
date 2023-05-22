86090b70f0f1a4e9e46003c49eb1a3f91ca5455b
{ Walyson } <walysonmoura222@gmail.com>
2023-05-22 10:58:26 -0300

    new

--- src/components/Home/Categories/index.tsx
+++ src/components/Home/Categories/index.tsx
@@ -1,5 +1,48 @@
+"use client";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation, Pagination } from "swiper";
+
+import { categories } from "@/config/site";
 
 export function Categories() {
-    
+  return (
+    <Styles.CategoriesContainer
+      slidesPerView={4}
+      centeredSlides={true}
+      spaceBetween={30}
+      grabCursor={true}
+    >
+      {categories.map((category, index) => (
+        <SwiperSlide key={index}>
+          <CategoryItem
+            title={category.title}
+            link={category.link}
+            image={category.image}
+          />
+        </SwiperSlide>
+      ))}
+    </Styles.CategoriesContainer>
+  );
+}
+
+function CategoryItem({ title, link, image }) {
+  return (
+    <Styles.CategoryItemContainer>
+      <Link href={link}>
+        <SlideImage
+          src={image}
+          width={1000}
+          height={1000}
+          alt={title || "Imagem da categoria"}
+        />
+      </Link>
+    </Styles.CategoryItemContainer>
+  );
 }
--- src/components/Home/Categories/styles.ts
+++ src/components/Home/Categories/styles.ts
@@ -1,3 +1,42 @@
-import { styled } from "styled-components";
+"use client"
 
-export const BestSellersContainer = styled.section``
+import Image from "next/image"
+import Link from "next/link"
+import { styled } from "styled-components"
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import { Autoplay, Navigation, Pagination } from "swiper"
+
+import { siteConfig } from "@/config/site"
+
+
+export const CategoriesContainer = styled(Swiper)`
+width: 100%;
+  height: 400px;`;
+
+export const CategoryItemContainer = styled.div`
+
+text-align: center;
+  font-size: 18px;
+  background: #fff;
+  width: 5rem;
+  height: 8rem;
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+export const Image = styled(Image)`
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
