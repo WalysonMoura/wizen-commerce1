267c7803be1a8bb6de7fb67bf7aeb70b15c6a798
{ Walyson } <walysonmoura222@gmail.com>
2023-05-22 16:05:57 -0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { Categories } from "@/components/Home/Categories"
-import { BestSellers } from "@/components/Home/Sections/BestSellers"
-import { PersonalizedOffers } from "@/components/Home/Sections/PersonalizedOffers"
-import { Promotions } from "@/components/Home/Sections/Promotions"
-import { StoreNews } from "@/components/Home/Sections/StoreNews"
-import { Video } from "@/components/Home/Video"
-import SliderBanners from "@/components/Home/sliderBanners"
-import * as Styles from "@/styles/pages/home"
+import { Categories } from "@/components/Home/Categories";
+import { BestSellers } from "@/components/Home/Sections/BestSellers";
+import { PersonalizedOffers } from "@/components/Home/Sections/PersonalizedOffers";
+import { Promotions } from "@/components/Home/Sections/Promotions";
+import { StoreNews } from "@/components/Home/Sections/StoreNews";
+import { Video } from "@/components/Home/Video";
+import SliderBanners from "@/components/Home/sliderBanners";
+import * as Styles from "@/styles/pages/home";
 
-import { buttonVariants } from "../components/ui/button"
-import { siteConfig } from "../config/site"
+import { buttonVariants } from "../components/ui/button";
+import { siteConfig } from "../config/site";
 
 export default function IndexPage() {
   return (
@@ -34,9 +34,9 @@ export default function IndexPage() {
         <StoreNews />
         <PersonalizedOffers />
       </div>
-      <Styles.AboutUs>
+      <div>
         <Video />
-      </Styles.AboutUs>
+      </div>
 
       <div className="flex gap-4">
         <Link
@@ -58,5 +58,5 @@ export default function IndexPage() {
         </Link>
       </div>
     </section>
-  )
+  );
 }
