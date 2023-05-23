715568b7fff86f19b6a23e7780f5ead97551aa67
{ Walyson } <walysonmoura222@gmail.com>
2023-05-22 21:29:03 -0300

    new

--- src/components/ProductComponents/AddToCart.tsx
+++ src/components/ProductComponents/AddToCart.tsx
@@ -1,87 +1,85 @@
 // Imports
-import { useContext, useState } from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { v4 as uuidv4 } from 'uuid';
+import { useContext, useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { v4 as uuidv4 } from "uuid";
 
 // Components
 
-
 // State
-import { CartContext } from '@/contexts/CartProvider';
+import { CartContext } from "@/contexts/CartProvider";
 
 // Utils
-import { getFormattedCart } from '@/utils/functions/functions';
-import { GET_CART } from '@/utils/gql/GQL_QUERIES';
-import { ADD_TO_CART } from '@/utils/gql/GQL_MUTATIONS';
-import Button from './Button.component';
+import { getFormattedCart } from "@/utils/functions/functions";
+import { GET_CART } from "@/utils/gql/GQL_QUERIES";
+import { ADD_TO_CART } from "@/utils/gql/GQL_MUTATIONS";
+import Button from "./Button.component";
 
 // GraphQL
 
-
 interface IImage {
-    __typename: string;
-    id: string;
-    uri: string;
-    title: string;
-    srcSet: string;
-    sourceUrl: string;
+  __typename: string;
+  id: string;
+  uri: string;
+  title: string;
+  srcSet: string;
+  sourceUrl: string;
 }
 
 interface IVariationNode {
-    __typename: string;
-    name: string;
+  __typename: string;
+  name: string;
 }
 
 interface IAllPaColors {
-    __typename: string;
-    nodes: IVariationNode[];
+  __typename: string;
+  nodes: IVariationNode[];
 }
 
 interface IAllPaSizes {
-    __typename: string;
-    nodes: IVariationNode[];
+  __typename: string;
+  nodes: IVariationNode[];
 }
 
 export interface IVariationNodes {
-    __typename: string;
-    id: string;
-    databaseId: number;
-    name: string;
-    stockStatus: string;
-    stockQuantity: number;
-    purchasable: boolean;
-    onSale: boolean;
-    salePrice?: string;
-    regularPrice: string;
+  __typename: string;
+  id: string;
+  databaseId: number;
+  name: string;
+  stockStatus: string;
+  stockQuantity: number;
+  purchasable: boolean;
+  onSale: boolean;
+  salePrice?: string;
+  regularPrice: string;
 }
 
 interface IVariations {
-    __typename: string;
-    nodes: IVariationNodes[];
+  __typename: string;
+  nodes: IVariationNodes[];
 }
 
 export interface IProduct {
-    __typename: string;
-    id: string;
-    databaseId: number;
-    averageRating: number;
-    slug: string;
-    description: string;
-    onSale: boolean;
-    image: IImage;
-    name: string;
-    salePrice?: string;
-    regularPrice: string;
-    price: string;
-    stockQuantity: number;
-    allPaColors?: IAllPaColors;
-    allPaSizes?: IAllPaSizes;
-    variations?: IVariations;
+  __typename: string;
+  id: string;
+  databaseId: number;
+  averageRating: number;
+  slug: string;
+  description: string;
+  onSale: boolean;
+  image: IImage;
+  name: string;
+  salePrice?: string;
+  regularPrice: string;
+  price: string;
+  stockQuantity: number;
+  allPaColors?: IAllPaColors;
+  allPaSizes?: IAllPaSizes;
+  variations?: IVariations;
 }
 
 export interface IProductRootObject {
-    product: IProduct;
-    variationId?: number;
+  product: IProduct;
+  variationId?: number;
 }
 
 /**
@@ -91,68 +89,68 @@ export interface IProductRootObject {
  */
 
 const AddToCart = ({ product, variationId }: IProductRootObject) => {
-    const { setCart } = useContext(CartContext);
-    const [requestError, setRequestError] = useState<boolean>(false);
+  const { setCart } = useContext(CartContext);
+  const [requestError, setRequestError] = useState<boolean>(false);
 
-    const productId = product?.databaseId ? product?.databaseId : variationId;
+  const productId = product?.databaseId ? product?.databaseId : variationId;
 
-    const productQueryInput = {
-        clientMutationId: uuidv4(), // Generate a unique id.
-        productId,
-    };
+  const productQueryInput = {
+    clientMutationId: uuidv4(), // Generate a unique id.
+    productId,
+  };
 
-    // Get cart data query
-    const { data, refetch } = useQuery(GET_CART, {
-        notifyOnNetworkStatusChange: true,
-        onCompleted: () => {
-            // Update cart in the localStorage.
-            const updatedCart = getFormattedCart(data);
+  // Get cart data query
+  const { data, refetch } = useQuery(GET_CART, {
+    notifyOnNetworkStatusChange: true,
+    onCompleted: () => {
+      // Update cart in the localStorage.
+      const updatedCart = getFormattedCart(data);
 
-            if (!updatedCart) {
-                return;
-            }
+      if (!updatedCart) {
+        return;
+      }
 
-            localStorage.setItem('woocommerce-cart', JSON.stringify(updatedCart));
+      localStorage.setItem("woocommerce-cart", JSON.stringify(updatedCart));
 
-            // Update cart data in React Context.
-            setCart(updatedCart);
-        },
-    });
+      // Update cart data in React Context.
+      setCart(updatedCart);
+    },
+  });
 
-    // Add to cart mutation
-    const [addToCart, { loading: addToCartLoading }] = useMutation(ADD_TO_CART, {
-        variables: {
-            input: productQueryInput,
-        },
+  // Add to cart mutation
+  const [addToCart, { loading: addToCartLoading }] = useMutation(ADD_TO_CART, {
+    variables: {
+      input: productQueryInput,
+    },
 
-        onCompleted: () => {
-            // Update the cart with new values in React context.
-            refetch();
-        },
+    onCompleted: () => {
+      // Update the cart with new values in React context.
+      refetch();
+    },
 
-        onError: () => {
-            setRequestError(true);
-        },
-    });
+    onError: () => {
+      setRequestError(true);
+    },
+  });
 
-    const handleAddToCart = () => {
-        addToCart();
-        // Refetch cart after 2 seconds
-        setTimeout(() => {
-            refetch();
-        }, 2000);
-    };
+  const handleAddToCart = () => {
+    addToCart();
+    // Refetch cart after 2 seconds
+    setTimeout(() => {
+      refetch();
+    }, 2000);
+  };
 
-    return (
-        <>
-            <Button
-                handleButtonClick={() => handleAddToCart()}
-                buttonDisabled={addToCartLoading || requestError}
-            >
-                KJØP
-            </Button>
-        </>
-    );
+  return (
+    <>
+      <Button
+        handleButtonClick={() => handleAddToCart()}
+        buttonDisabled={addToCartLoading || requestError}
+      >
+        Adicionar ao Carrinho
+      </Button>
+    </>
+  );
 };
 
 export default AddToCart;
