4b2ff3512a588d75b9b97f23435d082c7e26f251
{ Walyson } <walysonmoura222@gmail.com>
2023-05-24 16:21:37 -0300

    new

--- src/components/header/AccountMenu/index.tsx
+++ src/components/header/AccountMenu/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Settings2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
--- src/components/header/MobileMenu/styles.ts
+++ src/components/header/MobileMenu/styles.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { styled } from "styled-components";
 
 export const HamburgerButton = styled.input.attrs({ type: "checkbox" })`
