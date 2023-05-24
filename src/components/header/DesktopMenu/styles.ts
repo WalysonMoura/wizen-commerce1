a84464d0c2252a88ef4f3ef1f48cb90d7042fe2a
{ Walyson } <walysonmoura222@gmail.com>
2023-05-24 14:52:28 -0300

    new

--- src/components/header/DesktopMenu/styles.ts
+++ src/components/header/DesktopMenu/styles.ts
@@ -12,7 +12,11 @@ align-items: center;
 justify-content: space-between;`;
 export const Account = styled.div`7 display: flex;
 align-items: center;
-justify-content: space-between;`;
+justify-content: space-between;
+strog{
+  color:  ${({ theme }) => theme.colors.primary.lightBlue};
+}
+`;
 
 export const WizenPrimeLink = styled.a`
   display: flex;
